6226b3aad46aadf92be64c1e16c49be725234b9b
rakamiracle <rakasabri6@gmail.com>
2024-11-04 12:55:44 +0700

    push 3

--- myapp/routes/example.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET users listing. */
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-module.exports = router;
